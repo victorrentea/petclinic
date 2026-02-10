dc34d75579d4c67b7e57420213703a634e60bc36
Victor Rentea <victorrentea@gmail.com>
2026-02-11 00:17:24 +0200

    added stub calculator

---
+++ spring-petclinic-rest/src/main/java/org/springframework/samples/petclinic/util/Calculator.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.petclinic.util;
+
+public class Calculator {
+    int calculate(String s) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ spring-petclinic-rest/src/test/java/org/springframework/samples/petclinic/util/CalculatorTest.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.petclinic.util;
+
+class CalculatorTest {
+    
+    // https://kata-log.rocks/string-calculator-kata
+
+
+}
