1124bb455e509fc7071f9e471c9b6eb8e198720d
Victor Rentea <victorrentea@gmail.com>
2026-03-10 23:34:53 +0200

    undo calculator

--- petclinic-frontend/src/app/calculator/calculator.spec.ts
+++ petclinic-frontend/src/app/calculator/calculator.spec.ts
@@ -2,19 +2,4 @@
 
 import { add } from './calculator';
 
-describe('String Calculator', () => {
-    it('should return 0 for empty string', () => {
-        expect(add("")).toBe(0);
-    });
-    it('should return the number for a single number string', () => {
-        expect(add("1")).toBe(1);
-    });
-    it('should return the sum of two numbers in a string', () => {
-        expect(add("1,2")).toBe(3);
-    });
-    it('should return the sum of multiple numbers in a string', () => {
-        expect(add("1\n2,3")).toBe(6);
-    });
 
-
-});
--- petclinic-frontend/src/app/calculator/calculator.ts
+++ petclinic-frontend/src/app/calculator/calculator.ts
@@ -1,14 +1,6 @@
 
-export function add(s: string): number {
-    if (s === "") {
-        return 0;
-    }
-    if (s.includes(",")) {
-        const numbers = s.split(",").map(Number);
-        return numbers.reduce((a, b) => a + b, 0);
-    }
-
-    return parseInt(s);
+export function add(str: string): number {
+    throw new Error('Not implemented');
 }
 
 
