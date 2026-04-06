88b960ecbe271204bd12ee9d39f268d8848845d8
Victor Rentea <victorrentea@gmail.com>
2026-04-06 10:15:25 +0300

    vibe

--- petclinic-frontend/src/app/owners/pagination-control/pagination-control.component.ts
+++ petclinic-frontend/src/app/owners/pagination-control/pagination-control.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination-control',
-  templateUrl: './pagination-control.component.html'
+  templateUrl: './pagination-control.component.html',
+  styleUrls: ['./pagination-control.component.css']
 })
 export class PaginationControlComponent {
   @Input() currentPage = 0;
