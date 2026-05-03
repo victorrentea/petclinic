2db3d26d4423d214743d7bcbad61a7ce3bff03de
Victor Rentea <victorrentea@gmail.com>
2026-05-03 15:51:15 +0300

    feat(visits): route /visits to VisitsPageComponent

--- petclinic-frontend/src/app/visits/visits-routing.module.ts
+++ petclinic-frontend/src/app/visits/visits-routing.module.ts
@@ -1,24 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {VisitListComponent} from './visit-list/visit-list.component';
+import {VisitsPageComponent} from './visits-page/visits-page.component';
 import {VisitEditComponent} from './visit-edit/visit-edit.component';
 import {VisitAddComponent} from './visit-add/visit-add.component';
 
 const visitRoutes: Routes = [
-  {path: 'visits', component: VisitListComponent},
+  {path: 'visits', component: VisitsPageComponent},
   {path: 'visits/add', component: VisitAddComponent},
-  {path: 'visits/:id/edit', component: VisitEditComponent}
-
+  {path: 'visits/:id/edit', component: VisitEditComponent},
 ];
 
-
 @NgModule({
-  imports: [
-    RouterModule.forChild(visitRoutes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forChild(visitRoutes)],
+  exports: [RouterModule],
 })
 export class VisitsRoutingModule {
 }
